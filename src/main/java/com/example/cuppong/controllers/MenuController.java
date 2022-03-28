d139ce22ec386d07ef89fb6ca39e0c84b648bead
rmr84 <73509790+rmr84@users.noreply.github.com>
2022-03-28 18:44:48 -0400

    background image on home screen

--- src/main/java/com/example/cuppong/controllers/MenuController.java
+++ src/main/java/com/example/cuppong/controllers/MenuController.java
@@ -1,11 +1,24 @@
 package com.example.cuppong.controllers;
 
+import com.example.cuppong.CupPongMain;
 import com.example.cuppong.util.StageManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 
 public class MenuController {
+    @FXML
+    VBox box;
+    @FXML
+    public void initialize(){
+        BackgroundImage myBI= new BackgroundImage(new Image(CupPongMain.class.getResourceAsStream("images/background.jpg")),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+//then you set to your node
+        box.setBackground(new Background(myBI));
+    }
 
     @FXML
     public void onBtnPlayClick(ActionEvent actionEvent) {
