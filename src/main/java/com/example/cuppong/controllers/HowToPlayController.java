238abb1f8a608383806e72602eee398cc0a0fe58
rmr84 <73509790+rmr84@users.noreply.github.com>
2022-03-29 00:11:21 -0400

    added labels and bright pretty colors and bilitskis face

--- src/main/java/com/example/cuppong/controllers/HowToPlayController.java
+++ src/main/java/com/example/cuppong/controllers/HowToPlayController.java
@@ -8,6 +8,8 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class HowToPlayController {
 
@@ -28,21 +30,44 @@ public class HowToPlayController {
     @FXML
     Label g;
 
+
+
     @FXML
     VBox box;
     @FXML
     public void initialize() {
         BackgroundImage myBI= new BackgroundImage(new Image(CupPongMain.class.getResourceAsStream("images/bilitski.jpg")),
-                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER,
                 BackgroundSize.DEFAULT);
 
         box.setBackground(new Background(myBI));
 
-        a.setTextFill(Color.WHITE);
-        b.setTextFill(Color.WHITE);
+        top.setTextFill(Color.GREENYELLOW);
+        top.setStyle("-fx-font-weight: bold");
+        top.setFont(Font.font("Arial", 20));
+        a.setFont((Font.font("Arial", 20)));
+        a.setTextFill(Color.GREENYELLOW);
+        a.setStyle("-fx-font-weight: bold");
+        b.setTextFill(Color.GREENYELLOW);
+        b.setStyle("-fx-font-weight: bold");
+        b.setFont(Font.font("Arial", 20));
+        c.setTextFill(Color.GREENYELLOW);
+        c.setStyle("-fx-font-weight: bold");
+        c.setFont(Font.font("Arial", 20));
+        d.setTextFill(Color.GREENYELLOW);
+        d.setStyle("-fx-font-weight: bold");
+        d.setFont(Font.font("Arial", 20));
+        e.setTextFill(Color.GREENYELLOW);
+        e.setStyle("-fx-font-weight: bold");
+        e.setFont(Font.font("Arial", 20));
+        f.setTextFill(Color.GREENYELLOW);
+        f.setStyle("-fx-font-weight: bold");
+        f.setFont(Font.font("Arial", 20));
+        g.setTextFill(Color.GREENYELLOW);
+        g.setStyle("-fx-font-weight: bold");
+        g.setFont(Font.font("Arial", 20));
+
     }
-
-
 
     @FXML
     public void onBackClicked(ActionEvent actionEvent) {
